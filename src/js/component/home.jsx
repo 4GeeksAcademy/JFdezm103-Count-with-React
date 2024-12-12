a478e914ed7b940004b2dde57e630d5eebceaaba
Jennifer Fernández <j.fdezmurga@gmail.com>
2024-12-12 19:53:39 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,24 +3,39 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+
+
+const Home = (props) => {
+return (
+<div className="counter">
+	<div className="row bg-dark text-center text-white mt-4 ">
+		<div className="col p-2 mx-3 ms-5">
+			<i className="fa-regular fa-clock" style={{color: "white"}}></i>
 		</div>
-	);
+   		<div className="col p-2 mx-3">
+     	 	{props.num6}
+   		</div>
+    	<div className="col p-2 mx-3">
+			{props.num5}
+    	</div>
+    	<div className="col p-2 mx-3">
+			{props.num4}
+    	</div>
+		<div className="col p-2 mx-3">
+			{props.num3}
+   		</div>
+    	<div className="col p-2 mx-3">
+			{props.num2}
+    	</div>
+    	<div className="col p-2 ms-3 me-5">
+			{props.num1}
+    	</div>
+  </div>
+</div>
+);		
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,50 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+let num4 = 0;
+let num5 = 0;
+let num6 = 0;
 
+
+setInterval(function() {
+    num1++;
+ 
+if (num1 > 9) {
+    num1 = 0;
+    num2++;
+}
+
+if (num2 > 9) {
+    num2 = 0;
+    num3++;
+}
+
+if (num3 > 9) {
+    num3 = 0;
+    num4++;
+}
+
+if (num4 > 9) {
+    num4 = 0;
+    num5++;
+}
+
+if (num5 > 9) {
+    num5 = 0;
+    num6++;
+}
+
+if (num6 > 9) {
+    num6 = 0;
+    num7++;
+}
+
+
+//render your react application
+ReactDOM.createRoot(document.getElementById('app')).render (<Home num1={num1} num2={num2} num3={num3} num4={num4} num5={num5} num6={num6}/>);
+}, 1000);
+
+ReactDOM.createRoot(document.getElementById('app')).render (<Home/>);
